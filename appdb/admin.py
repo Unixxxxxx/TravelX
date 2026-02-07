d88322179d97f3ad4c0b56539072b91e4ce09924
Unixxxxxx <sudhanshuroyss208@gmail.com>
2026-02-07 16:22:18 +0530

    Html*Changes

--- appdb/admin.py
+++ appdb/admin.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
-from .models import Contact
+from .models import Contact, new 
 
 @admin.register(Contact)
 class ContactAdmin(admin.ModelAdmin):
     list_display = ('name', 'email', 'created_at')
     search_fields = ('name', 'email')
     list_filter = ('created_at',)
-
