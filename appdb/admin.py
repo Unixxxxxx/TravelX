65cbc0bed2f121b696a49888f0caba6d3ffbe75d
Unixxxxxx <sudhanshuroyss208@gmail.com>
2026-02-07 16:46:20 +0530

    admin*Changes**

--- appdb/admin.py
+++ appdb/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from .models import Contact, new 
 
+
+admin.site.site_header = "TraveX"
+admin.site.site_title = "TravelX Admin Poetal"
+admin.site.index_title = "Welcome to TravelX Dashboard"
 @admin.register(Contact)
 class ContactAdmin(admin.ModelAdmin):
     list_display = ('name', 'email', 'created_at')
